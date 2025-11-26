2d5fd39b236e32b6b5ccc222d1f6b7706caabd12
Meesa000 <104768879+Meesa000@users.noreply.github.com>
2025-11-26 16:53:01 +0000

    add explanation of code

--- server.py
+++ server.py
@@ -9,19 +9,23 @@ def server(socket):
     socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("Server started.")
 
+    # binds socket obj to port and ip, then listens for any incoming conns
     socket.bind((HOST_CONNECTION,PORT))
     socket.listen()
+    # if conn then accepts it, returns a conn and addr tuple
     conn, addr = socket.accept()
     print(f"Connection established with {addr}")
 
     try:
+        # if up to 1024 bytes are received then adds it to data variable  then decodes it 
+        # into a string
         data = conn.recv(1024)
         data_decoded = data.decode('utf-8')
         print(data_decoded)
 
     except OSError as e:
         print(f"Error: {e}")
-        
+
     socket.close()
     
 
